334a226e1aebec650f871b9cebbfb630b873ba2e
Malcolm Yeh <malcolmyeh@gmail.com>
2020-10-06 18:03:07 -0400

    Updates

---
+++ precache-manifest.6443a4f8adb22c4340ab2ac025f0c84b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02ac94ee155dbbd7d8c7992c55db9705",
+    "url": "/moviedatabase/index.html"
+  },
+  {
+    "revision": "48d088c9f8f985cd3af7",
+    "url": "/moviedatabase/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "6380f61d004d98d1a09d",
+    "url": "/moviedatabase/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "48d088c9f8f985cd3af7",
+    "url": "/moviedatabase/static/js/2.23680e1b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/moviedatabase/static/js/2.23680e1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6380f61d004d98d1a09d",
+    "url": "/moviedatabase/static/js/main.53711588.chunk.js"
+  },
+  {
+    "revision": "694edc8bb620f3405bb5",
+    "url": "/moviedatabase/static/js/runtime-main.eb53d2ca.js"
+  }
+]);
